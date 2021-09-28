e935ef990ad754477e47732376d2a27347408ec1
xmr <91093705+neutralising@users.noreply.github.com>
2021-09-28 16:47:32 -0400

    Update 0x0.js

--- assets/0x0.js
+++ assets/0x0.js
@@ -7,7 +7,7 @@ function addwindow() {
     document.removeEventListener("click", addwindow, null)
     let boux = new WinBox({
         title: window.location.host,
-        html: '<link rel="stylesheet" href="css/index.css"><div id="text"><h1>[pooython@host ~]# █</h1><h3 style="color: #9cecfc;" onclick=window.open("https://github.com/Recognising/images/")>Github</h3><h3 style="color: #9cecfc;" onclick=window.open("https://vuln.cf/cfg")>Config</h3><h3 style="color: #9cecfc;" onclick=window.open("https://raw.githubusercontent.com/Recognising/images/main/domains")>Domains</h3></div>'
+        html: '<link rel="stylesheet" href="assets/index.css"><div id="text"><h1>[pooython@host ~]# █</h1><h3 style="color: #9cecfc;" onclick=window.open("https://github.com/Recognising/images/")>Github</h3><h3 style="color: #9cecfc;" onclick=window.open("https://vuln.cf/cfg")>Config</h3><h3 style="color: #9cecfc;" onclick=window.open("https://raw.githubusercontent.com/Recognising/images/main/domains")>Domains</h3></div>'
     })
 }
 
